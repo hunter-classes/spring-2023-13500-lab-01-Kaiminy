1f373f2921bfdd559abb74c3f6b2b1e7de05c6b4
Kaiminy <104949057+Kaiminy@users.noreply.github.com>
2023-01-29 21:24:15 +0000

    added smaller3.cpp

---
+++ .ccls-cache/@home@runner@spring-2023-13500-lab-01-Kaiminy/smaller3.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int num1, num2, num3;
+  cout<<"Enter the first number: ";
+  cin>>num1;
+  cout<<"Enter the second number: ";
+  cin>>num2;
+  cout<<"Enter the third number: ";
+  cin>>num3;
+  
+  if (num1<num2 && num1<num2){
+    cout<< "Smallest number is " << num1;
+    } else if (num2<num1&&num2<num3){
+    cout <<"Smaller number is " <<num2;
+    }else {
+    cout<<"Smallest number is "<< num3;
+    }
+  return 0;
+    }
+   
